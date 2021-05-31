30bd65861c6aacf5440abd16840886380c9ba97c
eunsu20191222 <eunsuvg@gmail.com>
2021-05-31 21:52:17 +0800

    no message

--- main.py
+++ main.py
@@ -2,8 +2,5 @@ from time import sleep
 
 user = input("이름을 입력해 주세요: ") #유저네임 저장
 
-running = True
-while running:
-    print("hello")
-    if user == user:
-        break
+print(f"{user}님 안녕하세요!")
+print("게임 시작")
