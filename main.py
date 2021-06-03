7cbc3b73475aec54bfdc494ca3afb5e8b85bda3b
eunsu20191222 <eunsuvg@gmail.com>
2021-06-03 20:52:51 +0800

    final 그동안 수고 했음

--- main.py
+++ main.py
@@ -9,20 +9,23 @@ user = input("이름을 입력해 주세요: ") #유저네임 저장
 
 print(f"{user}님 안녕하세요!")
 print("게임 시작")
-
+point = 0
 while True:
     result = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"]
     final_result = random.choice(result)
     sleep(1)
+    print("point: ", point)
     print(final_result)
     ask = input(": ")
     if ask == final_result:
+        point += 1
         print("correct")
-        sleep(1)
+        sleep(0.5)
         clear()
     elif ask == "log out":
         break
     else:
+        point -= 1
         print("wrong")
         sleep(1)
         clear()
