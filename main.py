d6db00c355bd8d9ff1b07a049d10645054f3c0ea
eunsu20191222 <eunsuvg@gmail.com>
2021-06-03 20:56:46 +0800

    진짜끝

--- main.py
+++ main.py
@@ -7,22 +7,22 @@ def clear():
 
 user = input("이름을 입력해 주세요: ") #유저네임 저장
 
-print(f"{user}님 안녕하세요!")
+print(f"{user}님 안녕하세요!") #유저네임 프린트 (근데 구지 할 필요가 있나??)
 print("게임 시작")
-point = 0
+point = 0 #while 문 안쪽에다 하면 계속 리셋 되서 점수가 안올라감
 while True:
-    result = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"]
+    result = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
     final_result = random.choice(result)
     sleep(1)
     print("point: ", point)
     print(final_result)
     ask = input(": ")
-    if ask == final_result:
+    if ask == final_result: #input 과 result의 랜덤이 같으면 정답 아니면 틀림
         point += 1
         print("correct")
         sleep(0.5)
         clear()
-    elif ask == "log out":
+    elif ask == "log out": #로그 아웃을 치면 게임 종료
         break
     else:
         point -= 1
